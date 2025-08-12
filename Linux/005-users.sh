b95ec48df86764391ffa37f4ad562383321d7541
Cristian Fernández Kittler <cfk2424@gmail.com>
2025-08-12 15:45:11 -0300

    005 Table improvements+Doc

--- Linux/005-users.sh
+++ Linux/005-users.sh
@@ -59,7 +59,7 @@ generate_data() {
                 # Extraer estado (L=bloqueada, P=activa, NP=sin contraseña) 
                 password_state=$(echo "$password_info" | awk '{print $2}')  
                 case "$password_state" in
-                    "L") password_status="❌ BLOQ ❌";;
+                    "L") password_status="🔴 BLOQUE 🔴";;
                     "P") password_status="🟢 ACTIVA 🟢";;
                     "NP") password_status="🔓 SIN PASS 🔓";;
                     *) password_status="❓ $password_state";;
